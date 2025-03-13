d2bb949ccbb0b3837e0118267bfb777224470589
ChamodyaGit <backchamodyapunsara@gmail.com>
2025-03-14 00:28:52 +0530

    feat: sidebar changes

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,5 @@
 <template>
-    <h1>Modal Dialogs</h1>
-    <p>
-        Use these Tailwind CSS modal dialog components to create pop-up boxes for things like alerts, notifications,
-        or confirmation prompts. These modals are designed and built by the Tailwind CSS team, and include a variety
-        of different styles and layouts.
-    </p>
+    
     <div class="home-container">
         <h2>Simple with gray footer</h2>
 
